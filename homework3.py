a48a68a4943d5113d136152ffbebc9eb861420b0
simondvdv <simondv4@gmail.com>
2024-05-28 00:51:44 +0300

    Add files via upload
    28.05 Домашняя работа по уроку "Динамическая типизация"

---
+++ homework3.py
@@ -0,0 +1,8 @@
+name = 'Александр'
+print('\nИмя: ' + name)
+age = 31
+print('Возраст: ' + str(age))
+age = 31 + 1    # С днем рождения меня!!!
+print('Новый возраст: ' + str(age))
+is_student = True
+print('Студент: ' + str(is_student))
